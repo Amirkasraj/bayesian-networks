550075f00f853f40cd6617050f00247a00ec889d
Unknown <amirkasraj@yahoo.com>
2018-08-08 22:51:28 +0430

    Auto stash before merge of "master" and "origin/master"

---
+++ ToolsForSamIam/DataForSamiam/dataloader.py
@@ -0,0 +1,16 @@
+# Modules
+import pandas as pd
+
+# Method
+## Iran Loader
+def loader_iran():
+    #
+    
+    # Return
+    return
+
+## Quandl Loader
+def loader_quandl():
+    
+    # Return
+    return
